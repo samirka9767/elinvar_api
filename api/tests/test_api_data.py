f8555310dc20a8249ad760d38ef3e0a937dbab37
Samira <samirehuseyinli@gmail.com>
2022-02-13 23:46:22 +0400

    add more tests

--- api/tests/test_api_data.py
+++ api/tests/test_api_data.py
@@ -1,12 +1,16 @@
 import unittest
 import requests
 import configparser
+from datetime import date
+
 
 config = configparser.ConfigParser()
 config.read('config/api_uri.ini')
 config = config['api_uri']
 get_uri = "{}{}".format(config["API_URL"], config["GET_TEST_RESULTS"])
 post_uri = "{}{}".format(config["API_URL"], config["POST_TEST_RESULTS"])
+
+
 
 
 class APIData(unittest.TestCase):
@@ -23,6 +27,14 @@ class APIData(unittest.TestCase):
         res = requests.get(get_uri, params={'squad': 'incorrect_name'})
         self.assertEqual(len(res.json()['details']), 3)
 
-    def test_add_test_reports(self):
-        res = requests.post(post_uri, params={'squad': 'happy', 'status': 'failed', 'failedTests': 3})
-        self.assertEqual(res.status_code, 200)
+    def test_incorrect_status_bad_request(self):
+        res = requests.post(post_uri, params={'squad': 'happy', 'status': 'wrongStatus', 'failedTests': 0})
+        self.assertEqual(res.status_code, 400)
+
+    def test_add_test_reports_all_params(self):
+        res = requests.post(post_uri, params={'squad': 'luna_team', 'status': 'failed', 'failedTests': 93})
+        self.assertEqual(str(date.today().strftime("%m/%d/%Y")), res.json()['date'])
+
+
+
+
--- api/tests/test_api_status.py
+++ api/tests/test_api_status.py
@@ -3,20 +3,24 @@ import requests
 import configparser
 
 config = configparser.ConfigParser()
-config.read('../config/api_uri.ini')
+config.read('config/api_uri.ini')
 config = config['api_uri']
 get_uri = "{}{}".format(config["API_URL"], config["GET_TEST_RESULTS"])
 post_uri = "{}{}".format(config["API_URL"], config["POST_TEST_RESULTS"])
 
 
 class APITest(unittest.TestCase):
-    print(config)
+
     def test_get_all_test_reports(self):
         res = requests.get(get_uri)
         self.assertEqual(res.status_code, 200)
 
-    def test_add_tests_with_all_parameters(self):
-        res = requests.post(get_uri, params={'squad': 'happy', 'status': 'failed', 'failedTests': 5})
+    def test_get_all_test_reports_with_parameters(self):
+        res = requests.get(get_uri)
+        self.assertEqual(res.status_code, 200)
+
+    def test_add_test_reports_all_params(self):
+        res = requests.post(post_uri, params={'squad': 'happy', 'status': 'failed', 'failedTests': 3})
         self.assertEqual(res.status_code, 200)
 
     def test_no_failedTests_success(self):
@@ -30,3 +34,5 @@ class APITest(unittest.TestCase):
     def test_no_status_bad_request(self):
         res = requests.post(post_uri, params={'squad': 'happy', 'failedTests': 0})
         self.assertEqual(res.status_code, 400)
+
+
